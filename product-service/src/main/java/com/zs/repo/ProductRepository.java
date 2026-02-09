220a7ede9d005dd1785ed038a72aaffff0ab382b
zubairify <41644191+zubairify@users.noreply.github.com>
2026-02-09 17:16:51 +0530

    Product micro-service repo update

--- product-service/src/main/java/com/zs/repo/ProductRepository.java
+++ product-service/src/main/java/com/zs/repo/ProductRepository.java
@@ -3,7 +3,6 @@ package com.zs.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.zs.entity.Product;
 
